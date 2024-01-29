74064d243f372eff82857f68700c13d665f19e4d
spiecee <spiecee@rose-hulman.edu>
2024-01-29 10:58:44 -0500

    Test commit from Ethan

--- src/mainApp/GameMain.java
+++ src/mainApp/GameMain.java
@@ -17,7 +17,8 @@ public class GameMain {
 	private void runApp() {	
 		JFrame frame = new JFrame("JETPACK JOYRIDE!!!");
         frame.setSize(1000, 500);
-        System.out.println("H");
+        //Test Commit
+        System.out.println("This works");
         
         
         //TODO: add components and timer here
