ea1723282107bd6e83f3e2684b1071803f1de325
strawmbj <strawmbj@137.112.219.195>
2024-01-29 23:59:30 -0500

    created ElectricBarrier class

--- src/mainApp/Barrier.java
+++ src/mainApp/Barrier.java
@@ -10,17 +10,48 @@ public class Barrier {
     private int width;
     private int height; 
 
-    public Barrier(int x, int y, int width, int height, boolean isElectrified) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    public Barrier(int x, int y, int width, int height) {
+        this.setX(x);
+        this.setY(y);
+        this.setWidth(width);
+        this.setHeight(height);
     }
 
-    //draws the barrier object
     public void draw(Graphics g) {
         g.setColor(Color.GRAY); 
-        g.fillRect(x, y, width, height);
+        g.fillRect(getX(), getY(), getWidth(), getHeight());
     }
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
 
 }
--- src/mainApp/ElectricBarrier.java
+++ src/mainApp/ElectricBarrier.java
@@ -1,9 +1,19 @@
 package mainApp;
 
-public class ElectricBarrier {
+import java.awt.Color;
+import java.awt.Graphics;
 
-	public ElectricBarrier() {
-		// TODO Auto-generated constructor stub
+public class ElectricBarrier extends Barrier {
+
+
+	public ElectricBarrier(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		
 	}
 
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(Color.ORANGE); 
+        g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
 }
